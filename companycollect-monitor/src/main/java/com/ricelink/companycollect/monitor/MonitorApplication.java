bc1e976bdf75dce5d4600554e40e5ca477debb04
cn <xxx@163.com>
2019-03-12 23:07:11 +0800

    监控中心配置修改

--- companycollect-monitor/src/main/java/com/ricelink/companycollect/monitor/MonitorApplication.java
+++ companycollect-monitor/src/main/java/com/ricelink/companycollect/monitor/MonitorApplication.java
@@ -3,6 +3,9 @@ package com.ricelink.companycollect.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableAdminServer
 @SpringBootApplication
@@ -10,6 +13,18 @@ public class MonitorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonitorApplication.class, args);
+    }
+
+    /**
+     * For the sake of brevity we’re disabling the security for now. Have a look at the security section on how to deal with secured endpoints.
+     */
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
     }
 
 }
