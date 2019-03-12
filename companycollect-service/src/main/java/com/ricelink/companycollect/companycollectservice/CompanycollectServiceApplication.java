b7bb6882ba080667ff0173d5c0f8e04f1faecace
595980161@qq.com <chenning>
2019-03-12 16:46:22 +0800

    新建module项目

---
+++ companycollect-service/src/main/java/com/ricelink/companycollect/companycollectservice/CompanycollectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ricelink.companycollect.companycollectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanycollectServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanycollectServiceApplication.class, args);
+    }
+
+}
---
+++ companycollect-service/src/test/java/com/ricelink/companycollect/companycollectservice/CompanycollectServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ricelink.companycollect.companycollectservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompanycollectServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
